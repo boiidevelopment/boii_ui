d8f0ee6afe8b388b2be2b461a8c31ce28a9f0061
Case <90377400+CaseIRL@users.noreply.github.com>
2023-05-15 18:57:56 +0100

    Update contextmenu_cl.lua

--- client/contextmenu/contextmenu_cl.lua
+++ client/contextmenu/contextmenu_cl.lua
@@ -7,7 +7,7 @@ local menu_options = {
     {
         header = 'Option 1', -- Menu option header if header is being used
         header_icon = '<i class="fa-solid fa-bell"></i>', -- Menu header icon if using one this can be removed if not
-        image = '/html/assets/images/lspd.png', -- Menu header image if using one if not remove
+        image = '/html/assets/images/logo.png', -- Menu header image if using one if not remove. For images stored elsewhere make sure to use `nui://your_resource/` to path to the images correctly.
         message = 'A menu option with icons and a image that triggers a server event.', -- Menu option text if text is being used
         message_icon = '<i class="fa-solid fa-bell"></i>', -- Menu header icon if using one this can be removed if not
         action_type = 'server_event', -- Type to trigger on click this can be removed. Actions: 'client_event', 'server_event'
@@ -19,7 +19,9 @@ local menu_options = {
     {
         header = 'Option 2', -- Menu option header if header is being used
         header_icon = '<i class="fa-solid fa-bell"></i>', -- Menu header icon if using one this can be removed if not
+        --image = '/html/assets/images/logo.png', -- Menu header image if using one if not remove. For images stored elsewhere make sure to use `nui://your_resource/` to path to the images correctly.
         message = 'A menu option with a icon that triggers a client event.', -- Menu option text if text is being used
+        --message_icon = '<i class="fa-solid fa-bell"></i>', -- Menu header icon if using one this can be removed if not
         action_type = 'client_event', -- Type to trigger on click this can be removed. Actions: 'client_event', 'server_event'
         action = 'boii_ui:cl:menu_test_event',  -- Name of event to trigger
         params = {}, -- Event params
@@ -28,7 +30,10 @@ local menu_options = {
     },
     {
         header = 'Option 3', -- Menu option header if header is being used
+        --header_icon = '<i class="fa-solid fa-bell"></i>', -- Menu header icon if using one this can be removed if not
+        --image = '/html/assets/images/lspd.png', -- Menu header image if using one if not remove. For images stored elsewhere make sure to use `nui://your_resource/` to path to the images correctly.
         message = 'A menu option that triggers a client event and closes the menu.', -- Menu option text if text is being used
+        --message_icon = '<i class="fa-solid fa-bell"></i>', -- Menu header icon if using one this can be removed if not
         action_type = 'client_event', -- Type to trigger on click this can be removed. Actions: 'client_event', 'server_event'
         action = 'boii_ui:cl:menu_test_event', -- Name of event to trigger
         params = {},  -- Event params
@@ -44,7 +49,7 @@ local menu_data = {
         image = '/html/assets/images/lspd.png', -- Image to display on header. Remove this to use no image
         icon = '<i class="fa-solid fa-bell"></i>' -- Icon to display next to header text. Remove this to use no icon
     },
-    menu_options = menu_options, -- Menu options, refer to menu_options above, menu was split for readability 
+    menu_options = menu_options,
     menu_buttons = { -- Menu buttons mostly pointless since they work the same as any other options mainly just allows for more customisation over menu templates
         close = {
             use = true, -- Toggle the close button
@@ -57,12 +62,12 @@ local menu_data = {
 }
 
 -- Function to open menu
-local function open_menu(style, menu_data)
+local function open_menu(style, menu)
     SetNuiFocus(true, true)
     SendNUIMessage({
         action = 'create_menu',
         type = style, 
-        menu = table.clone(menu_data)
+        menu = table.clone(menu)
     })
 end
 
@@ -87,15 +92,9 @@ end)
 -- Context menu exports
 exports('open_menu', open_menu)
 
--- Test commands/events
 RegisterCommand('testmenu', function()
     SetNuiFocus(true, true)
-    open_menu('default', menu_data)
-end)
-
-RegisterCommand('testmenu2', function()
-    SetNuiFocus(true, true)
-    open_menu('lspd', menu_data)
+    exports['boii_ui']:open_menu('default', menu_data)
 end)
 
 RegisterNetEvent('boii_ui:cl:menu_test_event')
@@ -103,5 +102,3 @@ AddEventHandler('boii_ui:cl:menu_test_event', function()
     print('client event option clicked')
     exports['boii_ui']:notify(nil, 'client event triggered', 'success', 3500)
 end)
-
-
