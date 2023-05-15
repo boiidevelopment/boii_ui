8f05e16900178f3fab01d548194650302ecc3970
Case <90377400+CaseIRL@users.noreply.github.com>
2023-05-15 19:09:23 +0100

    Update print_sv.lua

--- server/print_sv.lua
+++ server/print_sv.lua
@@ -10,3 +10,9 @@ AddEventHandler('onResourceStart', function(resourceName)
     print('^5--<^3!^5>-- ^7BOII ^5| ^7DEVELOPMENT ^5--<^3!^5>-- ^7UTILITY: UI ELEMENTS V0.0.3 ^5--<^3!^5>--^7')
 end)
 --<!>-- SERVER PRINT --<!>--
+
+-- Server-side event to print "server event option clicked"
+RegisterServerEvent('boii_ui:sv:menu_test_event')
+AddEventHandler('boii_ui:sv:menu_test_event', function()
+    TriggerClientEvent('boii_ui:notify', source, nil, 'server event triggered', 'success', 3500)
+end)
